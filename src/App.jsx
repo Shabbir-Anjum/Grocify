97408c3ed7b8237f5c5b0692c196fd75b59d8dba
Shabbir-Anjum <mahrshabbir768@gmail.com>
2024-03-01 21:27:41 +0500

    dfd

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './Header'
+import Hero from './Hero'
 
 function App() {
 
   return (
     <>
   <Header/>
+  <Hero/>
     </>
   )
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <section className='flex justify-between gap-4'>
+        <div>
+          dfdf
+        </div>
+        <div>
+          <img src="/public/images/grocery-image.png" alt="" />
+        </div>
+    </section>
+  )
+}
