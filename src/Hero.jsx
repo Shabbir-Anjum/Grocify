b6f4efa8304a7796de0493ef284680baf7df6ca7
Shabbir-Anjum <mahrshabbir768@gmail.com>
2024-03-03 23:57:53 +0500

    main

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <section className='flex justify-between items-center gap-4'>
+    <section className='flex justify-between gap-4 h-screen'>
         <div className=' flex-1'>
+          
           <h2>Grocries</h2>
           p
         </div>
         <div className=' flex-1'>
+
           <img src="/public/images/grocery-image.png" alt="error"/>
+
         </div>
     </section>
   )
